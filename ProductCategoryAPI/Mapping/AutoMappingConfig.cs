001e05c8413c1c2c6ef643dacee3ed8a098845a4
Emin Yasir Corut <85822420+EminYasir@users.noreply.github.com>
2024-10-31 16:20:06 +0300

    Update AutoMappingConfig.cs

--- ProductCategoryAPI/Mapping/AutoMappingConfig.cs
+++ ProductCategoryAPI/Mapping/AutoMappingConfig.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+using AutoMapper;
 using DtoLayer.DTOs.CategoryDto;
 using DtoLayer.DTOs.ProductDto;
 using DtoLayer.DTOs.RegisterDto;
@@ -11,8 +11,16 @@ namespace ProductCategoryAPI.Mapping
         public AutoMappingConfig()
         {
             CreateMap<Category, CategoryAddDto>().ReverseMap();
-            CreateMap<Category, CategoryDto>().ReverseMap();
-            CreateMap<Category, CategoryListWithCheapestProduct>().ReverseMap();
+            CreateMap<CategoryDto, Category>().ReverseMap()
+                .ForMember(dest=>dest.ProductCount, opt =>opt.MapFrom(src=>src.Products.Count()));
+
+            CreateMap<CategoryListWithCheapestProduct, Product>().ReverseMap()
+                .ForMember(dest => dest.categoryId, opt => opt.MapFrom(src => src.CategoryId))
+                .ForMember(dest => dest.categoryName, opt => opt.MapFrom(src => src.Category.CategoryName))
+                .ForMember(dest => dest.productId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.productPrice, opt => opt.MapFrom(src => src.Price))
+                .ForMember(dest => dest.productName, opt => opt.MapFrom(src => src.Name));
+
             CreateMap<Category, CategoryUpdateDto>().ReverseMap();
 
             CreateMap<User,RegisterDto>().ReverseMap()
@@ -23,7 +31,7 @@ namespace ProductCategoryAPI.Mapping
 
 
             CreateMap<Product, ProductAddDto>().ReverseMap();
-            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<ProductDto, Product>().ReverseMap().ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.CategoryName));
         }
     }
 }
